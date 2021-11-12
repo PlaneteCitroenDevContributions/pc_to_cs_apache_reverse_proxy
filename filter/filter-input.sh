c6d428f0bf5e673c431faf7ff8b469bb96329443
bernhara <raphael.bernhard@orange.fr>
2021-11-12 17:47:39 +0100

    use date in debug file name to prevent overlapping of names (with possible write acces if already exists)
    #20

--- filter/filter-input.sh
+++ filter/filter-input.sh
@@ -10,7 +10,7 @@ then
     if [[ "${FILTER_DEBUG}" == "file" ]]
     then
 	: ${DEBUG_ROOT_DIR:="${HERE}/DEBUG"}
-	_debug_dir_="${DEBUG_ROOT_DIR}/debug_request_$$"
+	_debug_dir_="${DEBUG_ROOT_DIR}/debug_request_$( date '+%s' )"
 	mkdir -m 777 -p "${_debug_dir_}"
 
 	: ${STDERR:="${_debug_dir_}/stderr.txt"}
