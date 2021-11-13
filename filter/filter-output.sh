2804beed65522efd04e6ab10de100556b0d1d62a
bernhara <raphael.bernhard@orange.fr>
2021-11-13 19:07:02 +0100

    import changes from filter-input

--- filter/filter-output.sh
+++ filter/filter-output.sh
@@ -4,19 +4,39 @@
 
 HERE=$( dirname "$0" )
 
+#
+# FIXME: the following code is a copy from filter-input
+#         SHARE THE CODE
+#
 if [[ -n "${FILTER_DEBUG}" ]]
 then
     set -x
     if [[ "${FILTER_DEBUG}" == "file" ]]
     then
 	: ${DEBUG_ROOT_DIR:="${HERE}/DEBUG"}
-	_debug_dir_="${DEBUG_ROOT_DIR}/debug_request_$$"
+	_debug_dir_="${DEBUG_ROOT_DIR}/debug_request_$( date '+%s' )"
 	mkdir -m 777 -p "${_debug_dir_}"
 
 	: ${STDERR:="${_debug_dir_}/stderr.txt"}
-	: ${trace_file:="${_debug_dir_}/trace.txt"}
-
+	if echo '' >> "${STDERR}"
+	then
+	    :
+	else
+	    # invalidate _debug_dir_
+	    _debug_dir_=''
+	    STDERR=/dev/stderr
+	fi
 	exec 2>>"${STDERR}"
+	
+	: ${trace_file:="${_debug_dir_}/trace.txt"}
+	if echo '' >> "${trace_file}"
+	then
+	    :
+	else
+	    # invalidate _debug_dir_
+	    _debug_dir_=''
+	    trace_file="/dev/stderr"
+	fi
     fi
 fi
 
@@ -63,5 +83,9 @@ fi
 
 cat "${corrected_response_file}"
 
-cp "${response_file}" "${corrected_response_file}" "${_debug_dir_}"
+if [[ -n "${_debug_dir_}" ]]
+then
+    cp "${response_file}" "${corrected_response_file}" "${_debug_dir_}"
+fi
+
 rm -f "${response_file}" "${corrected_response_file}"
